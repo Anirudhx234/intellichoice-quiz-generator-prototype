a92984993d3837b328d664b091257db77237c9e1
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2021-08-13 19:12:16 -0500

    back to browser router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './Home.js';
-import { HashRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, HashRouter, Switch, Route} from 'react-router-dom';
 import Addition from './pages/Addition.js';
 import Subtraction from './pages/Subtraction.js';
 import Multiplication from './pages/Multiplication.js';
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App" >
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Home />
           <Switch>
             <Route path="/addition" component={Addition} />
@@ -21,7 +21,7 @@ class App extends React.Component {
             <Route path="/division" component={Division} />
             <Route path="/fractions" component={Fractions} />
           </Switch>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     );
   }
